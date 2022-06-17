c86209212218e30e9efe70059566f0949f98f512
dev-kucha <dev@kucha.kiev.ua>
2022-06-17 21:36:12 +0000

    Deploying to gh-pages from @ dev-kucha/goit-js-hw-09@34d54f563a2b4f152f17e987de77ca65d3be2c20 🚀

--- 02-timer.413c5b74.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={datetimePicker:document.querySelector("#datetime-picker"),button:document.querySelector("[data-start]"),dataDays:document.querySelector("[data-days]"),dataHours:document.querySelector("[data-hours]"),dataMinutes:document.querySelector("[data-minutes]"),dataSeconds:document.querySelector("[data-seconds]")};console.log(e)}();
-//# sourceMappingURL=02-timer.413c5b74.js.map
