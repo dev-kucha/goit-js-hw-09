6365ecc7eebedd2c26a835099e6ce41fc6bdee41
Yurii <ukucher@gmail.com>
2022-06-17 23:35:35 +0300

    1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {};
+refs.dataStart = document.querySelector('button[data-start]');
+refs.dataStop = document.querySelector('button[data-stop]');
+refs.body = document.querySelector('body');
 
+refs.dataStart.addEventListener('click', onDataStartClick);
+
+refs.dataStop.addEventListener('click', onDataStopClick);
+
+function onDataStartClick() {
+  refs.dataStart.setAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onDataStopClick() {
+  clearInterval(timerId);
+  refs.dataStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
